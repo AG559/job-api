bd6f9a70cb3d22af569b2b63e347a30423c61f99
aungkyawkhaing <agmsx.938@gmail.com>
2022-01-16 17:57:12 +0630

    final setup

--- app.js
+++ app.js
@@ -9,12 +9,27 @@ const jobRoute = require('./routes/job');
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+// extra package
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rate_limitter = require('express-rate-limit');
 
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 // extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.set('trust proxy', 1)
+app.use(rate_limitter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+}));
 
 // routes
 app.use('/api/v1', authRoute);
